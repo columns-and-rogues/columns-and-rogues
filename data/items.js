5b6582790b9eb7539964d7f5673453a80ba61f04
sethripman <sethripman@gmail.com>
2019-11-18 14:34:10 -0800

    finished seed data

--- data/items.js
+++ data/items.js
@@ -1,66 +1,37 @@
 module.exports = [
     {
-        name: 'Felix',
-        type: 'Tuxedo',
-        url: 'assets/cats/felix.png',
-        year: 1892,
-        lives: 3,
-        isSidekick: false
+        id: 1,
+        name: 'Sword',
+        dice: 6,
+        effect: 'slice',
+        image: 'https://p7.hiclipart.com/preview/350/997/88/sword-dagger-ancient-egypt-greco-persian-wars-khopesh-sword-thumbnail.jpg',
     },
     {
-        name: 'Garfield',
-        type: 'Orange Tabby',
-        url: 'assets/cats/garfield.jpeg',
-        year: 1978,
-        lives: 7,
-        isSidekick: false
+        id: 2,
+        name: 'Big Sword',
+        dice: 8,
+        effect: 'slice',
+        image: 'https://p7.hiclipart.com/preview/350/997/88/sword-dagger-ancient-egypt-greco-persian-wars-khopesh-sword-thumbnail.jpg',
     },
     {
-        name: 'Duchess',
-        type: 'Angora',
-        url: 'assets/cats/duchess.jpeg',
-        year: 1970,
-        lives: 9,
-        isSidekick: false
+        id: 3,
+        name: 'Biggerest Sword',
+        dice: 10,
+        effect: 'slice',
+        image: 'https://p7.hiclipart.com/preview/350/997/88/sword-dagger-ancient-egypt-greco-persian-wars-khopesh-sword-thumbnail.jpg',
     },
     {
-        name: 'Stimpy',
-        type: 'Manx',
-        url: 'assets/cats/stimpy.jpeg',
-        year: 1990,
-        lives: 1,
-        isSidekick: true
+        id: 4,
+        name: 'Shield',
+        dice: 4,
+        effect: 'block',
+        image: 'https://p7.hiclipart.com/preview/350/997/88/sword-dagger-ancient-egypt-greco-persian-wars-khopesh-sword-thumbnail.jpg',
     },
     {
-        name: 'Sylvester',
-        type: 'Tuxedo',
-        url: 'assets/cats/sylvester.jpeg',
-        year: 1945,
-        lives: 1,
-        isSidekick: true
-    },
-    {
-        name: 'Tigger',
-        type: 'Orange Tabby',
-        url: 'assets/cats/tigger.jpeg',
-        year: 1928,
-        lives: 8,
-        isSidekick: false
-    },
-    {
-        name: 'Hello Kitty',
-        type: 'Angora',
-        url: 'assets/cats/hello-kitty.jpeg',
-        year: 1974,
-        lives: 9,
-        isSidekick: false
-    },
-    {
-        name: 'Hobbs',
-        type: 'Orange Tabby',
-        url: 'assets/cats/hobbs.jpeg',
-        year: 1985,
-        lives: 6,
-        isSidekick: true
+        id: 5,
+        name: 'Potion',
+        dice: 4,
+        effect: 'heal',
+        image: 'https://p7.hiclipart.com/preview/350/997/88/sword-dagger-ancient-egypt-greco-persian-wars-khopesh-sword-thumbnail.jpg',
     },
 ];
