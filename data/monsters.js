598dab93c4a54e42177e7ac87f12e67e4a9cc727
sethripman <sethripman@gmail.com>
2019-11-18 14:30:55 -0800

    wrote static seed for monsters

--- data/monsters.js
+++ data/monsters.js
@@ -1,66 +1,26 @@
 module.exports = [
     {
-        name: 'Felix',
-        type: 'Tuxedo',
-        url: 'assets/cats/felix.png',
-        year: 1892,
-        lives: 3,
-        isSidekick: false
+        id: 1,
+        name: 'Weak Cookie Monstah',
+        hp: 1,
+        dice: 4,
+        effect: 'hungry',
+        image: 'https://p7.hiclipart.com/preview/153/389/303/cookie-monster-elmo-zoe-big-bird-ernie-cookie-monster-thumbnail.jpg',
     },
     {
-        name: 'Garfield',
-        type: 'Orange Tabby',
-        url: 'assets/cats/garfield.jpeg',
-        year: 1978,
-        lives: 7,
-        isSidekick: false
+        id: 2,
+        name: 'Medium Cookie Monstah',
+        hp: 2,
+        dice: 6,
+        effect: 'real hungry',
+        image: 'https://p7.hiclipart.com/preview/153/389/303/cookie-monster-elmo-zoe-big-bird-ernie-cookie-monster-thumbnail.jpg',
     },
     {
-        name: 'Duchess',
-        type: 'Angora',
-        url: 'assets/cats/duchess.jpeg',
-        year: 1970,
-        lives: 9,
-        isSidekick: false
-    },
-    {
-        name: 'Stimpy',
-        type: 'Manx',
-        url: 'assets/cats/stimpy.jpeg',
-        year: 1990,
-        lives: 1,
-        isSidekick: true
-    },
-    {
-        name: 'Sylvester',
-        type: 'Tuxedo',
-        url: 'assets/cats/sylvester.jpeg',
-        year: 1945,
-        lives: 1,
-        isSidekick: true
-    },
-    {
-        name: 'Tigger',
-        type: 'Orange Tabby',
-        url: 'assets/cats/tigger.jpeg',
-        year: 1928,
-        lives: 8,
-        isSidekick: false
-    },
-    {
-        name: 'Hello Kitty',
-        type: 'Angora',
-        url: 'assets/cats/hello-kitty.jpeg',
-        year: 1974,
-        lives: 9,
-        isSidekick: false
-    },
-    {
-        name: 'Hobbs',
-        type: 'Orange Tabby',
-        url: 'assets/cats/hobbs.jpeg',
-        year: 1985,
-        lives: 6,
-        isSidekick: true
+        id: 3,
+        name: 'Mighty Cookie Monstah',
+        hp: 3,
+        dice: 8,
+        effect: 'hangry',
+        image: 'https://p7.hiclipart.com/preview/153/389/303/cookie-monster-elmo-zoe-big-bird-ernie-cookie-monster-thumbnail.jpg',
     },
 ];
