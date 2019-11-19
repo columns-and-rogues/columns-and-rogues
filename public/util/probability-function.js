1430b050c254d1a3e70afd075dc87856aa7d2bb1
sethripman <sethripman@gmail.com>
2019-11-18 16:34:54 -0800

    working on function

--- public/util/probability-function.js
+++ public/util/probability-function.js
@@ -2,6 +2,7 @@
 
 function probabilityFunction(character) {
     const totalRemainingEvents = (character.goldTilesRemaining + character.itemTilesRemaining + character.monsterTilesRemaining);
+    const percentChanceOfEvent = (totalRemainingEvents / character.unknownTilesRemaining)
 }
 
 export default probabilityFunction;
@@ -11,4 +12,22 @@ export default probabilityFunction;
                 gold_tiles_remaining INTEGER NOT NULL,
                 item_tiles_remaining INTEGER NOT NULL,
                 monster_tiles_remaining INTEGER NOT NULL,
+
+                Things we may want in local state: player x+y position (default 0,0 on page load), number of ? squares (24 on page load, 0,0,0 is always the start point), remaining gold / monsters / items from database
+                Cell is an object with {x,y,contents}
+                ? Cell is {x, y, null}
+                Empty cell is {x,y,0}
+                Gold cell is {x,y,1}
+                Monster cell is {x,y,2}
+                Item cell is {x,y,3}
+
+                Player is at {1,1,0} and wants to move into {1,2,null} i.e. straight down. Player has an x and y in local state. On page load, this is set to x=0 y=0.
+                1. Check that it is a valid move (not outside the array)
+                2. Change the players x and y position
+                3. If the player is on a cell that is {player.x, player.y, null}
+                4. Count the ? cells, this = a variable called $remaining
+                5. Divide remaining I + remaining G + remaining M by $remaining
+                6. This gives us the percent chance that it is an empty square
+                7. Run a probability function - if the number is over that percent chance, roll randomly between gold, item, or monster and set the cell {x,y,THIS} to that number
+                8. Open the modal and run the appropriate event
 */
