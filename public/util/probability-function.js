fd72a41985398a703138c7c309284cf8041fa582
sethripman <sethripman@gmail.com>
2019-11-18 17:04:44 -0800

    finished prob function ready to write tests!

--- public/util/probability-function.js
+++ public/util/probability-function.js
@@ -2,32 +2,39 @@
 
 function probabilityFunction(character) {
     const totalRemainingEvents = (character.goldTilesRemaining + character.itemTilesRemaining + character.monsterTilesRemaining);
-    const percentChanceOfEvent = (totalRemainingEvents / character.unknownTilesRemaining)
+    const percentChanceOfEvent = (totalRemainingEvents / character.unknownTilesRemaining);
+
+    // if we have 8 gold/item/monsters remaining and 20 unexplored tiles, we have a 0.4 chance of hitting a g/i/m tile
+    // if empty cell return 0
+    if (Math.random() > percentChanceOfEvent) {
+        character.unknownTilesRemaining --;
+        return 0;
+    }
+    
+    // resolve which type of tile this is
+    const choiceArray = [];
+    for (let i = 0; i < character.goldTilesRemaining; i++) {
+        choiceArray.push('g');
+    }
+    for (let i = 0; i < character.itemTilesRemaining; i++) {
+        choiceArray.push('i');
+    }
+    for (let i = 0; i < character.monsterTilesRemaining; i++) {
+        choiceArray.push('m');
+    }
+
+    const randomChoice = choiceArray[Math.floor(Math.random() * choiceArray.length)];
+
+    randomChoice === 'g' ? character.goldTilesRemaining--
+        : randomChoice === 'i' ? character.itemTilesRemaining--
+            : randomChoice === 'm' ? character.itemTilesRemaining--
+                : console.log('error! seth messed up his function');
+
+    return randomChoice === 'g' ? 1
+        : randomChoice === 'm' ? 2
+            : randomChoice === 'i' ? 3
+                : console.log('error! seth messed up his function');
+
 }
 
 export default probabilityFunction;
-
-/*
-                explored_tiles VARCHAR(256) NOT NULL,
-                gold_tiles_remaining INTEGER NOT NULL,
-                item_tiles_remaining INTEGER NOT NULL,
-                monster_tiles_remaining INTEGER NOT NULL,
-
-                Things we may want in local state: player x+y position (default 0,0 on page load), number of ? squares (24 on page load, 0,0,0 is always the start point), remaining gold / monsters / items from database
-                Cell is an object with {x,y,contents}
-                ? Cell is {x, y, null}
-                Empty cell is {x,y,0}
-                Gold cell is {x,y,1}
-                Monster cell is {x,y,2}
-                Item cell is {x,y,3}
-
-                Player is at {1,1,0} and wants to move into {1,2,null} i.e. straight down. Player has an x and y in local state. On page load, this is set to x=0 y=0.
-                1. Check that it is a valid move (not outside the array)
-                2. Change the players x and y position
-                3. If the player is on a cell that is {player.x, player.y, null}
-                4. Count the ? cells, this = a variable called $remaining
-                5. Divide remaining I + remaining G + remaining M by $remaining
-                6. This gives us the percent chance that it is an empty square
-                7. Run a probability function - if the number is over that percent chance, roll randomly between gold, item, or monster and set the cell {x,y,THIS} to that number
-                8. Open the modal and run the appropriate event
-*/
