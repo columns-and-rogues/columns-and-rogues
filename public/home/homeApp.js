192d079a48c7a2b53634a5485f74845384a61d91
Caleb Pendergraft <cackmed@gmail.com>
2019-11-19 14:45:03 -0800

    rewrote instructions

--- public/home/homeApp.js
+++ public/home/homeApp.js
@@ -23,13 +23,16 @@ class App extends Component {
                         When you start your game, you will be placed at the starting point of the grid, from there you will explore the dangerous and unexplored wilderness around you.
                     </li>
                     <li>
-                    You can move via the arrow keys
+                    You can move your character buy pressing down the arrow keys.
                     </li>
                     <li> 
-                        Once you explore a tile you will have a chance to either encounter danger, treasure or nothing at all.
+                        Once you explore a tile you will have a chance to either encounter a monster, treasure or nothing at all.
                     </li>
                     <li>
-                        Your exit point is in a randomly generated location so explore away to find it.
+                        If you lose an encounter with a monster you will walk away from wounded losing one HP. Once you lose all of you HP you perish and find yourself in the Pits of Gehenna. 
+                    </li>   
+                    <li>
+                        In order to win the game you need to find the randomly placed exit point.
                     </li>
                 </ul>
                 </main>
