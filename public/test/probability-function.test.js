df384efde70ae95d63023a4fdc0850168451dc6c
sethripman <sethripman@gmail.com>
2019-11-18 17:26:05 -0800

    everything working huzzah

--- public/test/probability-function.test.js
+++ public/test/probability-function.test.js
@@ -20,3 +20,18 @@ test('should return 0 if there are no gold, items, or monsters remaining', funct
     // Make assertions about what is expected valid result
     assert.equal(expected, result);
 });
+
+/* For testing random output - change first three values to test each output.
+const testCharacter2 = {
+    goldTilesRemaining: 3,
+    itemTilesRemaining: 3,
+    monsterTilesRemaining: 3,
+    unknownTilesRemaining: 15,
+};
+
+console.log(probabilityFunction(testCharacter2));
+console.log(probabilityFunction(testCharacter2));
+console.log(probabilityFunction(testCharacter2));
+console.log(probabilityFunction(testCharacter2));
+console.log(probabilityFunction(testCharacter2));
+*/
