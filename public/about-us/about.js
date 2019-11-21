fe775f81d49684134689d528a2f38bcb8c3212f5
TravelFiend <mikegrace274@gmail.com>
2019-11-21 01:24:07 -0800

    about us components in place and styling started

--- public/about-us/about.js
+++ public/about-us/about.js
@@ -1,3 +1,4 @@
 import AboutApp from './aboutApp.js'; 
+
 const aboutApp = new AboutApp(); 
 document.body.appendChild(aboutApp.renderDOM()); 
--- public/about-us/aboutApp.js
+++ public/about-us/aboutApp.js
@@ -1,28 +1,27 @@
 import Component from '../Component.js'; 
 import Header from '../common/Header.js';
-// import Profiles from '../../data/profiles.js';
+import AboutUsList from './aboutUsList.js';
+import profiles from './profiles.js';
 
 class AboutApp extends Component {
 
     onRender(dom) {
         const header = new Header(); 
         dom.prepend(header.renderDOM());
-        
-        //const profile = new Profiles;
-        //profile.forEach(dom => dom.appendChild(dom));
-    
+
+        const team = dom.querySelector('.team-profiles');
+        const aboutUsList = new AboutUsList({ profiles });
+        team.appendChild(aboutUsList.renderDOM());
     }
 
 
     renderHTML() {
         return /*html*/`
-            <div>
-                <main>
-                    <section class ="team-profiles">
-                        Here are the Team Profiles
-                    </section>
-                </main>
-            </div>
+            <main>
+                <section class ="team-profiles">
+                    Here are the Team Profiles
+                </section>
+            </main>
         `;
     }
 }
