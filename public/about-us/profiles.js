45de8e8e00e09bd2ad23cbddb09f39f03a2084e3
TravelFiend <mikegrace274@gmail.com>
2019-11-21 18:00:05 -0800

    about styling getting closer

--- public/about-us/profiles.js
+++ public/about-us/profiles.js
@@ -5,13 +5,12 @@ const profiles = [
         linkedIn: 'https://www.linkedin.com/in/caleb-pendergraft-5ba932a7/',
         github: 'https://github.com/cackmed/',
         about: 'Caleb is from Portland, Oregon. When not learning to code he can be found walking his dog around Columbia Park, listening to history podcasts, or having lively arguments with his roommate'
-'
     }, 
     {
         name: 'Seth Ripman',
         image: 'placeholder',
         linkedIn: 'placeholder',
-        github: 'placeholder',
+        github: 'https://github.com/sethripman',
         about: 'placeholder'
     },
     {
@@ -26,20 +25,20 @@ const profiles = [
         image: 'placeholder',
         linkedIn: 'https://www.linkedin.com/in/travismolter/',
         github: 'https://github.com/treem0/',
-        about: 'placeholder'
+        about: 'Travis Molter is an outdoor and code enthusiast.'
     },
     {
         name: 'Aaron Glenn',
         image: 'placeholder',
-        linkedIn: 'placeholder',
-        github: 'placeholder',
-        about: 'placeholder'
+        linkedIn: 'https://www.linkedin.com/feed/',
+        github: 'https://github.com/AaronEdwardGlenn',
+        about: 'My name is Aaron and I know all the games. Professional gamer 100%.'
     },
     {
         name: 'Joel Durham',
         image: 'placeholder',
-        linkedIn: 'placeholder',
-        github: 'placeholder',
+        linkedIn: 'https://github.com/joelpdurham',
+        github: 'https://www.linkedin.com/in/joel-patrick-durham-16a8bab6/',
         about: 'placeholder'
     }
 ];
