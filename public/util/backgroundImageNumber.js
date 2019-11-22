e4c90da1dc875d1288647bed9b8b69c8742bfb18
joelpdurham <joeldur@gmail.com>
2019-11-22 10:51:45 -0800

    deleted comments

--- public/util/backgroundImageNumber.js
+++ public/util/backgroundImageNumber.js
@@ -1,10 +1,3 @@
-//To implament
-// 1. on GameApp, call this function near the top feeding it character.boardsSurvived
-// 2. pass that number down as props to Board
-// 3. in Board.js assign that prop to a const backgroundNum
-// 4. give the board element a class of background-${backgroundNum}
-// 5. in css file create differnt class selectors for 0-9. the only item in each should be background-image:
-
 export function backgroundImageNumber(boardsSurvived) {
     let bckdNum = boardsSurvived;
     while (bckdNum > 9) {
