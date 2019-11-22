3ce918d621e82387b8ac933306d5cd996d844610
TravelFiend <mikegrace274@gmail.com>
2019-11-22 11:39:19 -0800

    stuff

---
+++ public/common/Header.js
@@ -0,0 +1,24 @@
+import Component from '../Component.js';
+
+class Header extends Component {
+    renderHTML() {
+        const title =
+            this.props.title || 'Columns and Rogues';
+
+        return /*html*/ `
+        <header>
+            <div class = 'logo'><img src="./assets/rogue-logo.png">
+            </div>
+                <h1>${title}</h1>
+                    <nav>
+                        <a href="./auth.html">Login |</a>
+                        <a href="./game-index.html">  Game |</a>
+                        <a href="./about-us.html">About Us |</a>
+                        <a href="./">Rules</a>
+                    </nav>
+        </header>
+    `;
+    }
+}
+
+export default Header;
